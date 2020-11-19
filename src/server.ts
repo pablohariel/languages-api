3e678279904fe53309dca3520577de4b13ddc8e5
pablohariel <pablohhdr@gmail.com>
2020-11-19 10:19:33 -0300

    ESLint and Jest

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+  return res.json({ message: 'Hello World' })
 })
 
 app.listen(3333, () => {
-  console.log('server up at port 3333');
+  console.log('server up at port 3333')
 })
